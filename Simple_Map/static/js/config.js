ecd1e8c536c5fa2c4aeffd2754b7281b6fb8e0fa
mitchell-alexleigh <75285458+mitchell-alexleigh@users.noreply.github.com>
2021-03-28 13:30:35 -0500

    Delete config.js

--- Simple_Map/static/js/config.js
+++
@@ -1,4 +0,0 @@
-// API key
-const API_KEY = "pk.eyJ1IjoiYWxleGxtaXRjaGVsbCIsImEiOiJja21wa2V5NXYwaG9pMm9teXdxODFiNWN1In0.UX0pRQkhneUIvbIBw8eaww";
-// Add console.log to check to see if our code is working.
-console.log("configx.js is running");
