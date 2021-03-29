d329519971ddbb4a28759a343c57f5a5dea5e840
mitchell-alexleigh <75285458+mitchell-alexleigh@users.noreply.github.com>
2021-03-28 19:58:14 -0500

    update js

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,8 +1,8 @@
 // Add console.log to check to see if our code is working.
 console.log("logic.js is running");
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 // Alt to setVeiw used for multiple layers or background image 
 /* let map = L.map("mapid", {
@@ -18,3 +18,40 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 });
 // adds 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// Grabbing our GeoJSON data.
+/* L.geoJson(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    pointToLayer: function(feature, latlng) {
+        console.log(feature);
+        return L.marker(latlng)
+        .bindPopup("<h2>" + feature.properties.city + "</h2>");
+    }
+
+}).addTo(map); */
+L.geoJson(sanFranAirport, {
+    onEachFeature: function(feature, layer){
+        console.log(layer);
+        layer.bindPopup("<h4> Airport Code: " + feature.properties.faa + "</h4> <hr> <h4> Airport Name: "+feature.properties.name +"</h4>");
+    }
+}).addTo(map);
