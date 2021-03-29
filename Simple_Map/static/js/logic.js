5b11bf0e35e5d6116e4d666feeb027d5bcc869e1
mitchell-alexleigh <75285458+mitchell-alexleigh@users.noreply.github.com>
2021-03-28 21:02:46 -0500

    Update logic.js

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -17,24 +17,27 @@ streets.addTo(map);
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/mitchell-alexleigh/Mapping_Earthquakes/main/majorAirports.json";
 
-/* // Grabbing our GeoJSON data.
+// Grabbing our GeoJSON data.
+
 d3.json(airportData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
     
     L.geoJson(data).addTo(map);
-}); */
+});
 
+//skill drill to add info to markers for each data point
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+/* d3.json(airportData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
     L.geoJson(data, {
+        //add a popup marker that displays all airports' codes and names.
         onEachFeature: function(feature, layer){
             console.log(layer);
             layer.bindPopup("<h4> Airport Code: " + feature.properties.faa + "</h4> <hr> <h4> Airport Name: "+feature.properties.name +"</h4>");
         }
     }).addTo(map);
     
-});
+}); */
 
