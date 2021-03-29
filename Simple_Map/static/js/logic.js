3bd74a7d7537288c647b9494604771c995e2a5d9
mitchell-alexleigh <75285458+mitchell-alexleigh@users.noreply.github.com>
2021-03-28 20:59:30 -0500

    update logic.js and add d3  reference to index

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -4,11 +4,6 @@ console.log("logic.js is running");
 // Create the map object with center and zoom level.
 let map = L.map('mapid').setView([30, 30], 2);
 
-// Alt to setVeiw used for multiple layers or background image 
-/* let map = L.map("mapid", {
-    center: [40.7, -94.5],
-    zoom: 4}); */
-
 // creates tile layer that will be the background of the map.
 //for differnt map styles see step 8 of moduel 13.2.4
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -18,3 +13,28 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 });
 // adds 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/mitchell-alexleigh/Mapping_Earthquakes/main/majorAirports.json";
+
+/* // Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+    
+    L.geoJson(data).addTo(map);
+}); */
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+    L.geoJson(data, {
+        onEachFeature: function(feature, layer){
+            console.log(layer);
+            layer.bindPopup("<h4> Airport Code: " + feature.properties.faa + "</h4> <hr> <h4> Airport Name: "+feature.properties.name +"</h4>");
+        }
+    }).addTo(map);
+    
+});
+
